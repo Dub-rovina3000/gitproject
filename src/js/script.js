06c53ff324dea216f93c9bbaf882a553d083add0
katya <katya-p01@mail.ru>
2021-04-08 20:42:56 +0300

    forms added
    back added
    
    messages from user are sent to email
    
    scroll added

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 300,
         adaptiveHeight: true,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 10000,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftarrow.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow.svg"></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 769,
                 settings: {
                   dots: true,
                   arrows: false
@@ -35,6 +35,82 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal windows
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function validate_forms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите своё имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свой email",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        }); 
+    }
+
+    validate_forms('#order form');
+    validate_forms('#consultation-form');
+    validate_forms('#consultation form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+           $(this).find("input").val("");
+           $('#consultation, #order').fadeOut();
+           $('.overlay, #thanks').fadeIn('slow');           
+           $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // smooth scroll and page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href").offsetTop;
+        console.log(_href)
+        $("html, body").animate({scrollTop: _href+"px"});
+        return false;
+    });
 
 });
           
